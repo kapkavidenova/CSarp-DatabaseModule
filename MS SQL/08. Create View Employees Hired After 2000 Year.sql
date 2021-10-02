3315af33425db345ccdbc43c3bae7e4a3160a9ee
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-10-02 10:31:34 +0300

    Ex

---
+++ MS SQL/08. Create View Employees Hired After 2000 Year.sql
@@ -0,0 +1,9 @@
+--08. Create View Employees Hired After 2000 Year
+CREATE VIEW V_EmployeesHiredAfter2000 AS
+(
+	SELECT FirstName,LastName
+		FROM Employees
+		WHERE YEAR(HireDate)>2000
+)
+
+--SELECT * FROM V_EmployeesHiredAfter2000
