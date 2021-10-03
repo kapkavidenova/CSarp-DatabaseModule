a10ae21d3f6b288bd098d278a950633fc363a72b
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-10-03 10:51:16 +0300

    Ex

---
+++ MS SQL/BuiltInFunctions/11. Find All Employees with Rank 2.sql
@@ -0,0 +1,10 @@
+--11. Find All Employees with Rank 2 *
+ SELECT *
+ FROM (
+	SELECT EmployeeID,FirstName,LastName,Salary,
+	DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+    FROM Employees
+    WHERE Salary BETWEEN 10000 AND 50000) AS [Table Rank]
+    WHERE [Rank] = 2
+	ORDER BY Salary DESC
+ 
