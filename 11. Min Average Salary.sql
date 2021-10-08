5ddd56488286aa7bf1cf9dc183aa1158007e79df
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-10-08 17:21:16 +0300

    Delete 11. Min Average Salary.sql

--- 11. Min Average Salary.sql
+++
@@ -1,7 +0,0 @@
---11. Min Average Salary
-SELECT MIN(avsal.AverageSalary) AS MinAverageSalary FROM
-	(
-		SELECT AVG(e.Salary) AS AverageSalary
-		FROM Employees AS e
-		GROUP BY e.DepartmentID
-    ) AS avsal
