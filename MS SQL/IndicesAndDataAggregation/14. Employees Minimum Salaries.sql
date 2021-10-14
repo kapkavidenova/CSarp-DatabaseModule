9b18593506e017be5bf09077a0dd44eabf57e97e
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-10-14 23:23:23 +0300

    Data Aggregation

---
+++ MS SQL/IndicesAndDataAggregation/14. Employees Minimum Salaries.sql
@@ -0,0 +1,7 @@
+--14. Employees Minimum Salaries
+SELECT DepartmentID,
+	MIN(Salary) AS [MinimumSalary]
+ FROM Employees
+
+ WHERE HireDate >'2000-01-01' AND DepartmentID IN (2,5,7)
+ GROUP BY DepartmentID
---
+++ MS SQL/IndicesAndDataAggregation/16. Employees Maximum Salaries.sql
@@ -0,0 +1,7 @@
+--16. Employees Maximum Salaries
+SELECT DepartmentID,MAX(Salary) AS [MaxSalary]
+
+FROM Employees
+
+GROUP BY DepartmentID
+HAVING MAX(Salary) <30000 OR MAX(Salary)>70000
