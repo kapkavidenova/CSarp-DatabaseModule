68a7a650efc15d0ce43fc436e7084a6d6d51a629
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-11-09 22:05:28 +0200

    Delete Entity-Framework-Core/Entity Relations directory

--- Entity-Framework-Core/Entity Relations/P01_StudentSystem.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P01_StudentSystem.Data
-{
-    public static class Configuration
-    {
-        public const string CONNECTION_STRING = @"Server=.;Database=StudentSystem;Integrated Security=True;";
-    }
-}
