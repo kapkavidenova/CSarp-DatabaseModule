cf405bbec8efb078e46855496c1aa6fb9ce0cee3
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-11-09 22:03:42 +0200

    EF Core

---
+++ Entity-Framework-Core/Entity Relations/P01_StudentSystem.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_StudentSystem.Data
+{
+    public static class Configuration
+    {
+        public const string CONNECTION_STRING = @"Server=.;Database=StudentSystem;Integrated Security=True;";
+    }
+}
